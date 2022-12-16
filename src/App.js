a9da146793af035d7db5e241f84dfe31060e0f86
abdulsagheer <abdulsagheeras29@gmail.com>
2022-12-17 00:40:57 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Gmail from "./components/Gmail/Gmail";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        <Gmail />
+      </h1>
+    </div>
+  );
+}
+
+export default App;
